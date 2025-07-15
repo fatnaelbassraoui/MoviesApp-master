1b966bc56577a26e4edfbbe8356a2cfde90c7b94
Fatna El bassraoui <elbassraouifatna4@gmail.com>
2025-07-15 13:41:30 +0200

    added new folders

---
+++ nativewind-demo/app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MovieDEtails = () => {
+  return (
+    <View>
+      <Text>MovieDEtails</Text>
+    </View>
+  );
+};
+
+export default MovieDEtails;
