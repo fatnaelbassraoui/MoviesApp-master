17c176e12186c8e48fd437875c2d295fa2dbd79b
Fatna El bassraoui <elbassraouifatna4@gmail.com>
2025-07-15 15:10:34 +0200

    implemented routing sistem

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,58 @@
-import { View, Text, ImageBackground, Image } from "react-native";
-import React from "react";
-import { Tabs } from "expo-router";
+import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, ImageBackground, Text, View } from "react-native";
+
+//Routing sistem
+const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground source={images.highlight} className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
+        {" "}
+        {/* overflow-hidden utility to clip any content within an element that overflows the bounds of that element */}
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
+      </ImageBackground>
+    );
+  }
+  return (
+    <View className="flex size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+};
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          borderRadius: 50,
+          marginHorizontal: 10,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "0f0d23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <>
-              <ImageBackground source={images.highlight} className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center">
-                <Image source={images.home} tintColor="#151312" className="size-5" />
-                <Text>Home</Text>
-              </ImageBackground>
-            </>
-          ),
+          tabBarIcon: ({ focused }) => <TabIcon focused={focused} icon={icons.home} title="Home" />,
         }}
       />
       <Tabs.Screen
@@ -26,13 +60,24 @@ const _layout = () => {
         options={{
           title: "Search",
           headerShown: false,
+          tabBarIcon: ({ focused }) => <TabIcon focused={focused} icon={icons.search} title="Search" />,
         }}
       />
       <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => <TabIcon focused={focused} icon={icons.save} title="Saved" />,
+        }}
+      />
+
+      <Tabs.Screen
         name="profile"
         options={{
-          title: "Prifile",
+          title: "Profile",
           headerShown: false,
+          tabBarIcon: ({ focused }) => <TabIcon focused={focused} icon={icons.person} title="Profile" />,
         }}
       />
     </Tabs>
