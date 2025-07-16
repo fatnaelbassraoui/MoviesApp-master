7a9c550b1ae8c5ebc09147d21ffeb3322dfde396
Fatna El bassraoui <elbassraouifatna4@gmail.com>
2025-07-16 16:18:45 +0200

    created useFeth component

--- app/services/api.tsx
+++ app/services/api.tsx
@@ -9,6 +9,7 @@ export const TMDB_CONFIG = {
 
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${TMDB_CONFIG.BASE_URL}/discover/movies?sort_by=popularity.desc`;
+
   const response = await fetch(endpoint, {
     method: "GET",
     headers: TMDB_CONFIG.headers,
