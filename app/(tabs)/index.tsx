bb399f7fabdb3f1803a2cac6e5c9410ae8e26ddb
Fatna El bassraoui <elbassraouifatna4@gmail.com>
2025-07-16 16:03:27 +0200

    created fetch data component, and search component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,23 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
-
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
+import { Image, ScrollView, View } from "react-native";
+import SearchBar from "../components/searchBar";
 export default function Index() {
+  const router = useRouter(); //hook allows you to move between screens programmatically
   return (
-    <View className="flex-1 items-center justify-center ">
-      <Text className="text-dark-200 font-bold text-5xl">Welcome!</Text>
+    <View className="flex-1 bg-primary ">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
+        <Image
+          source={icons.logo}
+          className="w-20 h-14 mt-20 mb-5 
+       mx-auto " //  to center logo
+        />
+        <View className="flex-1 mt-5">
+          <SearchBar onPress={() => router.push("/search")} placeholder="Search for a movie" />
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
           200: "#A8B5DB",
           300: "#9CA4AB",
         },
-        DARK: {
+        dark: {
           100: "#221f3d",
           200: "#0f0d23",
         },
